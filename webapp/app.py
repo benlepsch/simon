cf87b149c9ab47c8f129c844567a189ac7da4e58
Ben Lepsch <blepsch@gmail.com>
2024-01-09 16:09:37 -0500

    refoac

---
+++ webapp/app.py
@@ -0,0 +1,39 @@
+from flask import Flask
+from flask import render_template, url_for, redirect
+from flask import session, request
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base)
+
+app = Flask(__name__)
+app.secret_key = 'bomboclaat'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
+db.init_app(app)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+        <form method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
