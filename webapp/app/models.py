8117afeddd034606f3bd611afb1e88defe85c2f3
Ben Lepsch <blepsch@gmail.com>
2024-06-07 11:24:23 -0400

    add route class (?)

--- webapp/app/models.py
+++ webapp/app/models.py
@@ -6,6 +6,15 @@ class User(db.Model):
     password = db.Column(db.String(64))
     highest_grade = db.Column(db.String(7))
 
-# 5.12a
-# 1234567
-# 5.11a/b
+
+class Route(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64)) # "Guide Route Left",
+    area = db.Column(db.String(256)) # "1) Super Mario Boulder > Beauty Mountain > New River Gorge Proper > The New River Gorge Region > West Virginia",
+    mp_link = db.Column(db.String(128)) # https://www.mountainproject.com/route/105944154/guide-route-left,2.0,-1,
+    _type = db.Column(db.String(32)) # "Trad, TR",
+    yds = db.Column(db.String(16)) # 5.10a/b+ PG13,
+    pitches = db.Column(db.Integer) # 1
+    length = db.Column(db.Integer) # 30
+    lat = db.Column(db.Float) # 38.05531,
+    long = db.Column(db.Float) # -81.0362
